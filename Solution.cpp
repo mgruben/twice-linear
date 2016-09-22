3a0d6d392fa2e73a1eb7f60e514994243aa58673
mgruben <GrubenM@gmail.com>
2016-09-22 15:04:39 -0500

    Add population of set

--- Solution.cpp
+++ Solution.cpp
@@ -8,6 +8,14 @@ class DoubleLinear
 public:
     static int dblLinear(int n) {
         if (n == 0) return 1;
+        set<int> s;
+        s.insert(1);
+        while (s.size() < n) {
+            for (int i: s) {
+                s.insert(2*i + 1);
+                s.insert(3*i + 1);
+            }
+        }
     }
 };
 
