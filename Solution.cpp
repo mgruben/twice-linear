1f62a29560e720c37581a0045c0c24f888c28b12
mgruben <GrubenM@gmail.com>
2016-09-23 10:35:39 -0500

    Implement clever pq_only solution
    from https://www.codewars.com/kata/reviews/578cf68aa684bd8ed3000885/groups/57cdea6efa9fc5d6140001d9
    
    The idea behind this solution is that we don't actually care about our
    invariants; once we evaluate 1 (to obtain 3 and 4), we can toss 1
    aside, so long as we know know that there is a single invariant.
    Then, once we evaluate 3 (to obtain 7 and 10) and 4 (to obtain
    9 and 13), we can toss 3 and 4 aside, literally not caring what they were,
    so long as we now know that we have three total invariants.
    
    This matters because we care about the nth invariant.  Once we have
    n-1 invariants, we know that the next one in a sorted list of
    numbers to evaluate will be our nth invariant.
    
    Sorting here is key though, and is provided by the C++
    priority_queue data structure, which allows FIFO queue behavior,
    but which guarantees that the elements are sorted (here,
    in ascending order).
    
    Accordingly, although the numbers we are pushing to the pq
    are blended in with the numbers which are already there, the pq
    ensures that they are sorted, thus that the top() is the next
    invariant to evaluate, and thus we can just do n-1 iterations
    of popping and pushing two new numbers, secure in the knowledge
    that the next number to pop will be the nth invariant
    (That is, the nth term in the Twice Linear sequence this problem
    establishes).

--- Solution.cpp
+++ Solution.cpp
@@ -7,17 +7,6 @@
 
 using namespace std;
 
-string toString(set<long long unsigned int> s) {
-    if (s.size() == 0) return "()";
-    string ans = "(";
-    for (long long unsigned int i: s) {
-        ans += to_string(i);
-        ans += ", ";
-    }
-    ans = ans.substr(0, ans.length() - 2);
-    return ans + ")";
-}
-
 string toString(priority_queue<long long unsigned int,
             vector<long long unsigned int>,
             greater<long long unsigned int>> d) {
@@ -38,34 +27,18 @@ public:
         if (n == 0) return 1;
         priority_queue<long long unsigned int,
             vector<long long unsigned int>,
-            greater<long long unsigned int>> toEval;
-        set<long long unsigned int> evaled;
-        toEval.push(1);
-        int invariantSize = toEval.size();
-        int overlap = 1;
-        while(invariantSize < n) {
-            cout << "Begin insertion" << endl; // This is the slowest
-            for (int i = 0; i < invariantSize; i++) {
-                long long unsigned int num = toEval.top();
-                while (toEval.size() > 0 && num == toEval.top()) {
-                    toEval.pop(); // Handle upcoming duplicates
-                    i++; // Since list advancement
-                }
-                toEval.push(2*num + 1);
-                toEval.push(3*num + 1);
-                evaled.insert(num);
+            greater<long long unsigned int>> q;
+        q.push(1);
+        cout << "Begin insertion" << endl; // This is the slowest
+        for (int i = 0; i < n; i++) {
+            long long unsigned int num = q.top();
+            while (!q.empty() && num == q.top()) {
+                q.pop(); // Handle upcoming duplicates
             }
-            cout << "Begin overlap count" << endl;
-            invariantSize = evaled.size();
-            set<long long unsigned int>::iterator i;
-            i = evaled.lower_bound(toEval.top());
-            overlap = distance(i, evaled.end());
-            invariantSize -= overlap;
-            cout << *evaled.end() << endl;
+            q.push(2*num + 1);
+            q.push(3*num + 1);
         }
-        cout << "Begin vectorization" << endl;
-        vector<int> v(evaled.begin(), evaled.end());
-        return v[n];
+        return q.top();
     }
 };
 
