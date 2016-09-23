3ab91162d7762c227001fc20a52141afd7c31780
mgruben <GrubenM@gmail.com>
2016-09-23 10:42:23 -0500

    Add comments

--- Solution.cpp
+++ Solution.cpp
@@ -20,19 +20,31 @@ class DoubleLinear
 {
 public:
     static int dblLinear(int n) {
-        if (n == 0) return 1;
+        
+        // Handle malformed requests
+        if (n <= 0) return 1;
         priority_queue<long long unsigned int,
             vector<long long unsigned int>,
             greater<long long unsigned int>> q;
         q.push(1);
+        
+        /* Discard n-1 invariants, adding their children to q,
+         * which will be sorted because of priority_queue magic.
+         */
         for (int i = 0; i < n; i++) {
             long long unsigned int num = q.top();
             while (!q.empty() && num == q.top()) {
-                q.pop(); // Handle upcoming duplicates
+                
+                // Pop this entry and any subsequent duplicates
+                q.pop();
             }
             q.push(2*num + 1);
             q.push(3*num + 1);
         }
+        
+        /* This will be the next invariant to discard, hence it is
+         * the nth invariant, hence we return it.
+         */
         return q.top();
     }
 };
