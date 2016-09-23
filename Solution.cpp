857044dc39926061db85dd9a19c23df3065e14c6
mgruben <GrubenM@gmail.com>
2016-09-23 00:50:24 -0500

    Use sorted priority queue to save on sorting
    Previously, a deque was used, and sorting was taking most of the
    computational time.  Accordingly, sacrifice logarithmic input for
    linear min removal, and save on linearithmic sorting of a big
    deque.

--- Solution.cpp
+++ Solution.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <set>
 #include <vector>
-#include <deque>
+#include <queue>
 #include <algorithm>
 #include <math.h>
 
@@ -18,11 +18,12 @@ string toString(set<long long unsigned int> s) {
     return ans + ")";
 }
 
-string toString(deque<long long unsigned int> d) {
+string toString(priority_queue<long long unsigned int> d) {
     if (d.size() == 0) return "[]";
     string ans = "[";
-    for (long long unsigned int i: d) {
-        ans += to_string(i);
+    for (int i = 0; i < d.size(); i++) {
+        ans += to_string(d.top());
+        d.pop();
         ans += ", ";
     }
     return ans.substr(0, ans.length() - 2) + "]";
@@ -33,26 +34,24 @@ class DoubleLinear
 public:
     static int dblLinear(int n) {
         if (n == 0) return 1;
-        deque<long long unsigned int> toEval;
+        priority_queue<long long unsigned int,
+            vector<long long unsigned int>,
+            greater<long long unsigned int>> toEval;
         set<long long unsigned int> evaled;
-        toEval.push_back(1);
+        toEval.push(1);
         int invariantSize = toEval.size();
         int overlap = 1;
         while(invariantSize < n) {
-            cout << "Begin while" << endl;
             for (int i = 0; i < invariantSize; i++) {
-                long long unsigned int num = toEval.front();
-                toEval.pop_front();
-                toEval.push_back(2*num + 1);
-                toEval.push_back(3*num + 1);
+                long long unsigned int num = toEval.top();
+                toEval.pop();
+                toEval.push(2*num + 1);
+                toEval.push(3*num + 1);
                 evaled.insert(num);
             }
-            cout << "Begin sort" << endl;
-            sort(toEval.begin(), toEval.end()); // sort is the slowest
             invariantSize = evaled.size();
             set<long long unsigned int>::iterator i;
-            i = evaled.lower_bound(toEval.front());
-            cout << "Begin distance" << endl;
+            i = evaled.lower_bound(toEval.top());
             overlap = distance(i, evaled.end());
             invariantSize -= overlap;
         }
